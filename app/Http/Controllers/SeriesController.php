22ae408a269dd096476fc4e0930983f65a825871
Antonio Pontes <antonio.pontes@farmaciaindiana.com.br>
2022-12-04 18:34:18 -0300

    feat: cadastro de series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     public function index()
     {
-        $series = [1 => 'Serie 1', 2 => 'Serie 2', 3 => 'Serie 3'];
+        $series = Serie::all();
         return view('series.index')->with('series', $series);
     }
 
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        
+        return redirect('/series');
     }
 }
