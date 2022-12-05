01e08757fdd3a0b34a5cd0ae6756647aaa6822de
Antonio Pontes <antonio.pontes@farmaciaindiana.com.br>
2022-12-05 20:09:49 -0300

    feat: método de edição e atualização da série

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -30,11 +30,26 @@ class SeriesController extends Controller
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso!");
     }
 
-    public function destroy(Serie $series, Request $request)
+    public function destroy(Serie $series)
     {
         $series->delete();
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso!");
+    }
+
+    public function edit(Serie $series)
+    {
+        return view('series.edit')
+            ->with('serie', $series);
+    }
+
+    public function update(Serie $series, Request $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' editada com sucesso!");
     }
 }
